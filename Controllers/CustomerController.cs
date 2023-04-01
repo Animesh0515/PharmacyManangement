16d8738cfb13aa8204a9833a1a33f4ec84d29612
Animesh0515 <anigautam110@gmail.com>
2023-04-02 00:53:58 +0600

    Customer backend work completed

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,135 @@
+﻿using PharmacyManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http.Formatting;
+
+namespace PharmacyManagement.Controllers
+{
+    public class CustomerController : Controller
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ToString();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Adding new customer
+        [HttpPost]
+        public bool AddCustomer(CustomerModel customer)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "Insert into Customer (CustomerName,MobileNumber,Address,Email,DeletedFlag)Values('" + customer.CustomerName + "', '" + customer.MobileNumber + "','" + customer.Address + "','"+customer.Email+"','N')";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        //Fetching all data of Customer
+        public ActionResult GetCustomer()
+        {
+            List<CustomerModel> customerlist = new List<CustomerModel>(); //creating list for customer data
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "Select * from Customer where DeletedFlag='N'"; //getting all customer data that are not deleted. DeletedFlag='N' denotes not deleted.
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        using (SqlDataReader reader=cmd.ExecuteReader())
+                        {
+                            while(reader.Read())
+                            {
+                                CustomerModel customer = new CustomerModel(); //creating customer object
+                                customer.CustomerId = int.Parse(reader[0].ToString());
+                                customer.CustomerName = reader[1].ToString();
+                                customer.MobileNumber = long.Parse(reader[2].ToString());
+                                customer.Address = reader[3].ToString();
+                                customer.Email = reader[4].ToString();
+                                customerlist.Add(customer);
+                            }
+                            return Json(customerlist, JsonRequestBehavior.AllowGet);
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(500, "An error occurred while retrieving the data. Please try again later.");            
+            }
+        }
+        [HttpPost]
+        public bool EditCustomer(CustomerModel customer)
+        {
+            try
+            {
+                using (SqlConnection conn= new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "Update Customer set CustomerName='"+customer.CustomerName+"', MobileNumber='"+customer.MobileNumber+"', Address='"+customer.Address+"',Email='"+customer.Email+"' where CustomerId='"+customer.CustomerId+"'";
+                    using (SqlCommand cmd= new SqlCommand(query,conn))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        [HttpPost]
+        public bool DeleteCustomer(int Id)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "Update Customer set DeletedFlag='Y' where CustomerId="+Id;
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Models/CustomerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmacyManagement.Models
+{
+    public class CustomerModel
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public long MobileNumber { get; set; } //used long here because int was not accepting 10 digit value
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
