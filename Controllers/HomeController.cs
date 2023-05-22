6f288649c5e6c1aa57b95bf5ec10acfcdb70b278
Animesh0515 <anigautam110@gmail.com>
2023-05-22 22:31:53 +0545

    Dashboard data fetch complete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PharmacyManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
@@ -23,13 +24,106 @@ namespace PharmacyManagement.Controllers
             return View();
         }
 
-        public void GetDashboardData()
+        public DashBoardModel GetDashboardData()
         {
+            DashBoardModel model= new DashBoardModel();
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 using (SqlCommand command= new SqlCommand())
                 {
-                    string queries = "Select Count(1) from Customer where DeletedFlag='N';Select Count(1) from Suppliers where DeletedFlag='N';Select Count(1) from Medicines where DeletedFlag='N'; Select MedicineName";
+                    string queries = "Select Count(1) from Customer where DeletedFlag='N';" +
+                        "Select Count(1) from Suppliers where DeletedFlag='N';" +
+                        "Select Count(1) from Medicines where DeletedFlag='N'; " +
+                        "Select Count(1) from Medicines where DeletedFlag='N' and Quantity<1; " +
+                        "Select Count(1) from CustomerPurchase where DeletedFlag='N'; " +
+                        "Select Count(1) from Purchase where DeletedFlag='N';" +
+                        "Select MedicineName, PackingType, mp.CreatedDate, ExpiryDate from Medicines m join MedicinePurchased mp on m.MedicineId=mp.MedicineId where ExpiryDate < CONVERT(DATE, GETDATE()) and m.DeletedFlag='N' and mp.DeletedFlag='N';" +
+                        "select MedicineName, mp.CreatedDate from Medicines m join MedicinePurchased mp on m.MedicineId=mp.MedicineId where mp.CreatedDate > DATEADD(DAY, -7, GETDATE());";
+
+                    command.CommandText = queries;
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            model.CustomerCount= reader.GetInt32(0);                            
+                        }
+
+                        // Move to the next result set
+                        reader.NextResult();
+
+                        // Process the second query result
+                        if (reader.Read())
+                        {
+                            model.SupplierCount = reader.GetInt32(0);
+                            
+                        }
+                       
+                        reader.NextResult();
+
+                        // Process the third query result
+                        if (reader.Read())
+                        {
+                            model.MedicineCount = reader.GetInt32(0);
+                            
+                        }              
+
+                        
+                        reader.NextResult();
+
+                        // Process the fourth query result
+                        if (reader.Read())
+                        {
+                            model.MedicineOutOfStock= reader.GetInt32(0);
+                            
+                        }
+                        
+                        reader.NextResult();
+
+                        // Process the fourth query result
+                        if (reader.Read())
+                        {
+                            model.SalesCount= reader.GetInt32(0);
+                            
+                        }
+                        
+                        reader.NextResult();
+
+                        // Process the fourth query result
+                        if (reader.Read())
+                        {
+                            model.PurchaseCount= reader.GetInt32(0);
+                            
+                        }
+                        
+                        reader.NextResult();
+
+                        // Process the fourth query result
+                        while (reader.Read())
+                        {
+                            ExpiredMedicine expiredMed= new ExpiredMedicine();
+                            expiredMed.MedicineName= reader.GetString(0);
+                            expiredMed.PackingType= reader.GetString(1);
+                            expiredMed.CreatedDate= reader.GetString(2);
+                            expiredMed.ExpiryDate= reader.GetString(3);
+                            
+                            model.ExpiredMedicines.Add(expiredMed);
+                        }
+                        
+                        reader.NextResult();
+
+                        // Process the fourth query result
+                        while (reader.Read())
+                        {
+                            MedicineModel medicine = new MedicineModel();
+                            medicine.MedicineName= reader.GetString(0);
+                            medicine.CreatedDate= reader.GetString(1);
+                            
+                            model.RecentMedicines.Add(medicine);
+                        }
+
+                    }
+
+                    return model;
                 }
             }
         }
---
+++ Models/DashBoardModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManagement.Models
+{
+    public class DashBoardModel
+    {
+        public int CustomerCount { get; set; }
+        public int SupplierCount { get; set; }
+        public int MedicineCount { get; set; }
+        public int MedicineOutOfStock { get; set; }
+        public int SalesCount { get; set; }
+        public int  PurchaseCount { get; set; }
+
+        public List<ExpiredMedicine> ExpiredMedicines { get; set; }
+        public List<MedicineModel> RecentMedicines { get; set; }
+
+
+    }
+
+    public class ExpiredMedicine
+    {
+        public string MedicineName { get; set; }
+        public string PackingType { get; set; }
+        public string CreatedDate { get; set; }
+        public string ExpiryDate { get; set; }
+    }
+}
