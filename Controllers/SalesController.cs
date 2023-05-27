3a6088e38a7a077bdf61144476950782c248ed2b
AmitShankhadev <amitbajra16@gmail.com>
2023-05-27 22:21:24 +0545

    BaseController added in sale scontroller

--- Controllers/SalesController.cs
+++ Controllers/SalesController.cs
@@ -12,7 +12,7 @@ using System.Web.Mvc;
 
 namespace PharmacyManagement.Controllers
 {
-    public class SalesController : Controller
+    public class SalesController : BaseController
     {
         string connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ToString();
 
